a30984f9c26050a234e9261ab65fd850abe35bbd
BraeTroutman <btroutma@redhat.com>
2024-09-12 17:08:45 -0400

    add go program for generating markdown from .cook files

---
+++ docs.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func Walker(pathmap map[string]string) filepath.WalkFunc {
+	return func(pathstr string, info fs.FileInfo, err error) error {
+		if info.IsDir() || filepath.Ext(pathstr) != ".cook" {
+			return nil
+		}
+		dir, cookfilename := filepath.Split(strings.Replace(pathstr, "recipes", "docs", 1))
+		if dir != "" {
+			if err := os.MkdirAll(dir, fs.ModePerm); err != nil {
+				return fmt.Errorf("failed to create dir %s: %w", dir, err)
+			}
+		}
+		cookcli := exec.Command("cook", "recipe", "-f", "markdown", pathstr)
+		var stdout strings.Builder
+		var stderr strings.Builder
+		cookcli.Stdout = &stdout
+		cookcli.Stderr = &stderr
+		if err := cookcli.Run(); err != nil {
+			return fmt.Errorf("failed to create markdown using cookcli: %w", err)
+		}
+		outbytes, err := io.ReadAll(strings.NewReader(stdout.String()))
+		if err != nil {
+			return fmt.Errorf("failed to read stdout of cookcli process for file %s: %w", cookfilename, err)
+		}
+		if err := os.WriteFile(strings.Replace(dir+cookfilename, ".cook", ".md", 1), outbytes, fs.ModePerm); err != nil {
+			return fmt.Errorf("failed to write stdout of cookcli process for file %s: %w", cookfilename, err)
+		}
+		return nil
+	}
+}
+
+func main() {
+	pathmap := make(map[string]string)
+	if err := filepath.Walk("recipes", Walker(pathmap)); err != nil {
+		log.Fatalf("failed to traverse cook files: %v", err)
+	}
+	log.Printf("%+v", pathmap)
+}
---
+++ docs/casseroles/Sushi Bake.md
@@ -0,0 +1,55 @@
+---
+servings:
+- 6
+---
+
+# 
+
+## Ingredients
+- *2 cups* rice (preferably sushi)
+- *2* salmon fillets
+- olive oil
+- curry powder
+- garlic powder
+- salt
+- *1 pound* imitation crab
+- *8 ounces* cream cheese (softened)
+- *1/2 cup* mayo
+- *1 teaspoon* garlic powder
+- *1/2 teaspoon* salt
+- *1/4 teaspoon* pepper
+- *2 tablespoons* rice vinegar
+- green onion
+- sliced cucumber
+- sriricha
+- furikake
+
+## Cookware
+- casserole dish
+
+## Steps
+1. preheat oven to 475 degrees
+
+2. cook rice (preferably sushi) according to package instructions
+
+3. while the rice is cooking, season salmon fillets with olive oil, curry
+powder, garlic powder, and salt
+
+4. bake the salmon in the oven for (salmon)15 minutes. Remove from oven and
+reduce oven temp to 350
+
+5. add salmon, shredded imitation crab, cream cheese (softened), mayo, garlic
+powder, salt, and pepper
+
+6. shred the salmon with a fork or your hands, and mix everything together
+thoroughly.
+
+7. When the rice is done, put it into a casserole dish and shake rice vinegar
+over it. mix and spread to fill the bottom of the dish
+
+8. top the rice with the salmon + crab mixture, and bake for (casserole)25
+minutes
+
+9. cut the casserole into slices and serve topped with green onion, sliced
+cucumber, sriricha, and furikake
+
